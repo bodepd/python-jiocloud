536cf2bf3c77befadb2eee62c346f290e08e0aa1
Dan Bode <bodepd@gmail.com>
2014-12-26 14:34:47 +0100

    remove python-etcd dep
    b/c it is no longer required

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     license='Apache 2.0',
-    keywords='etcd openstack cloud',
-    install_requires=['python-etcd', 'IPy', 'mock', 'python-novaclient', 'PyYAML', 'netifaces', 'consulate', 'tornado', 'python-hpilo', 'python-ironicclient'],
+    keywords='consul openstack cloud',
+    install_requires=['IPy', 'mock', 'python-novaclient', 'PyYAML', 'netifaces', 'consulate', 'tornado', 'python-hpilo', 'python-ironicclient'],
 )
